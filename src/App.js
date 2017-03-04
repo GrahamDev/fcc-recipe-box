2202fc5f81475e403d84b2fffddc2070dc3f5d0c
Graham Wood <graham@builtprecise.com>
2017-03-03 21:31:08 -0500

    Added functionality to add a recipe

--- src/App.js
+++ src/App.js
@@ -7,17 +7,29 @@ class App extends Component {
   constructor(props) {
     super(props);
 
+    this.addRecipe = this.addRecipe.bind(this);
+
     this.state = {
       selectedRecipe: null,
-      recipes: []
+      recipes: {}
     };
+  }
+
+  addRecipe(recipe) {
+    console.log(recipe);
+    const recipes = {...this.state.recipes};
+    const timestamp = Date.now();
+    recipes[`recipe-${timestamp}`] = recipe;
+    this.setState({ recipes });
   }
 
   render() {
     return (
       <div className="App">
         <p>Hello</p>
-        <RecipeList />
+        <RecipeList
+          addRecipe={this.addRecipe}
+        />
         <RecipeDetail />
       </div>
     );
--- src/RecipeList.js
+++ src/RecipeList.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import AddRecipeForm from './AddRecipeForm'
 
 class RecipeList extends React.Component {
   render () {
     return (
-      <h4>Recipe List</h4>
+      <div>
+        <h4>Recipe List</h4>
+        <AddRecipeForm addRecipe={this.props.addRecipe}/>
+      </div>
     )
   }
 }
